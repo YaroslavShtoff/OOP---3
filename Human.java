509aebedec403d3a0cd1aedf9d1c2548d5c4249b
yarik <yarikshtoda1988@gmail.com>
2022-12-12 17:21:02 +0300

    ДЗ ООР-3

---
+++ Human.java
@@ -0,0 +1,52 @@
+public class Human {
+private int yearOfBirth ;
+        String name;
+private String town;
+        String jobTitle;
+
+public Human( int yearOfBirth, String name, String town, String jobTitle) {
+        setYearOfBirth(yearOfBirth);
+        this.yearOfBirth = yearOfBirth;
+        if (yearOfBirth < 0) {
+        this.yearOfBirth = 0;
+        }
+        this.name = name;
+        if (name== null) {
+        this.name = "Информация не указана";
+        }
+        setTown(town);
+        this.town = town;
+        if (town == null) {
+        this.name = "Информация не указана";
+        }
+        this.jobTitle = jobTitle;
+        if (jobTitle == null) {
+        this.jobTitle = "Информация не указана";
+        }
+        }
+
+public int getYearOfBirth() {
+        return yearOfBirth;
+        }
+
+public void setYearOfBirth(int yearOfBirth) {
+        this.yearOfBirth = yearOfBirth;
+        if (yearOfBirth < 0) {
+        this.yearOfBirth = 0;
+        }
+        }
+
+public String getTown() {
+        return town;
+        }
+
+public void setTown(String town) {
+        this.town = town;
+        if (town == null)
+        this.name = "Информация не указана";;
+        }
+        void life() {
+        System.out.println(" Привет! Меня зовут" + name + ". Я из города " + town + ". Я родился в "
+        + yearOfBirth + " году. Я работаю на должности " + jobTitle + ". Будем знакомы!");
+        }
+        }
